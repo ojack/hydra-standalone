e5f23b9f53cc341c5d23850eb299bf98252493c8
Diego <diego.vid.eco@gmail.com>
2019-08-05 22:28:43 -0500

    Add clock

---
+++ hydra/app/extensions/clock.js
@@ -0,0 +1,47 @@
+class Clock {
+  constructor({
+    startAt = 0, 
+    countBy = 1,
+    countWith = null,
+    interval = 1000, 
+    onTick = () => {}
+  } = {}) {
+    this.currentVal = startAt
+    this.countBy = countBy
+    this.countWith = countWith 
+    ? countWith 
+    : last => last + countBy
+    this.interval = interval
+    this.onTick = onTick
+    this.counter = setInterval(
+      () => {
+        this.currentVal = this.countWith(this.currentVal)
+        this.onTick(this.currentVal)
+      }, 
+      interval) // maybe add option for setting interval with bpm, i.e. 1 is one beat, 0.5 half a beat, etc.
+  }
+
+  val() {
+    return this.currentVal
+  }
+
+  stop() {
+    clearInterval(this.counter)
+  }
+
+
+  setInterval(interval, countWith) {
+    this.stop()
+    return new Clock({
+      startAt: this.currentVal,
+      countBy: this.countBy,
+      countWith: countWith || this.countWith,
+      interval: interval,
+      onTick: this.onTick
+    })
+  }
+
+  reset(val) {
+    this.currentVal = val || 0
+  }
+}
